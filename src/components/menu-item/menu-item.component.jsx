fc0198e41dc2b8a63e051e9e589222d2332738a8
dbozarth <don@fuelcnc.com>
2022-02-26 10:12:46 -0600

    Modified Menu-Item Sass and fixed Menu-Item Component

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import './menu-item.styles.scss';
 
 const MenuItem = ({ title, imageUrl, size }) => (
-  <div className={`${size} menu_item`}>
+  <div className={`${size} menu-item`}>
     <div
       className='background-image'
       style={{
