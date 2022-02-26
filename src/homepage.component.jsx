ea222ee1e99aac2c4eca5b60204dbfce787d607c
dbozarth <don@fuelcnc.com>
2022-02-26 04:59:42 -0600

    Initial Commit

---
+++ src/homepage.component.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+
+const HomePage = () => (
+    <div className='homepage'>
+        <div className='directory_menu'>
+            <div className='menu_item'>
+                <div className='content'>
+                    <h1 className='title'>Hats</h1>
+                    <span className='subtitle'>Shop Now</span>
+                </div>
+            </div>
+            <div className='menu_item'>
+                <div className='content'>
+                    <h1 className='title'>Jackets</h1>
+                    <span className='subtitle'>Shop Now</span>
+                </div>
+            </div>
+            <div className='menu_item'>
+                <div className='content'>
+                    <h1 className='title'>Sneakers</h1>
+                    <span className='subtitle'>Shop Now</span>
+                </div>
+            </div>
+            <div className='menu_item'>
+                <div className='content'>
+                    <h1 className='title'>Womens</h1>
+                    <span className='subtitle'>Shop Now</span>
+                </div>
+            </div>
+            <div className='menu_item'>
+                <div className='content'>
+                    <h1 className='title'>Mens</h1>
+                    <span className='subtitle'>Shop Now</span>
+                </div>
+            </div>
+        </div>
+    </div>
+)
+
+export default HomePage;
